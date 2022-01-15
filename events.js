2033a9475fbc34a3d7b965a6d71573f2cdef3494
L1ghtingBolt <l1ghtingbolt.exploits@gmail.com>
2022-01-15 19:41:58 +0100

    I think this is better

--- events.js
+++ events.js
@@ -28,7 +28,7 @@ $jsBtn.addEventListener('click', () => {
 
 // ! Event listeners
 $skypackBtn.addEventListener('click', () => {
-    $js.trigger('keyboard', 'type', {text: `\nimport ${$skypackInp.value.replace('-', '').toLowerCase()} from 'https://cdn.skypack.dev/${$skypackInp.value}'`});
+    $js.trigger('keyboard', 'type', {text: `import ${$skypackInp.value.replace('-', '').toLowerCase()} from 'https://cdn.skypack.dev/${$skypackInp.value}'`});
 })
 
 $("#download").addEventListener("click", () => {
